5d2b1ea7c5175c7cef9eb0ce39b00e0441cba636
cstapiarubio <cstapiarubio@gmail.com>
2017-06-01 21:54:00 -0400

    trabajo final

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 /*
  * Archivo principal de JS
  */
+
+  (function main(){
+ 	var boxes= Array.from(document.getElementsByClassName('box-services'));
+ 	var modal=document.getElementById("box-services-modal");
+ 	var bodyModal, close;
+
+ 	boxes.forEach(function(box){
+
+ 		
+ 		box.addEventListener("click", function(){
+ 			modal.innerHTML="";
+
+ 			bodyModal=document.createElement("div");
+
+ 			bodyModal.classList.add("modal-body");
+
+ 			bodyModal.innerHTML=box.innerHTML;
+
+ 			modal.appendChild(bodyModal);
+
+ 			modal.classList.remove("hide");
+
+ 			close=document.createElement("div");
+ 			close.classList.add("close");
+ 			img=document.createElement("img");
+ 			img.setAttribute("src", "http://static1.squarespace.com/static/55a420c9e4b01d62e11ae524/t/573bf9cc8259b5b384b219e8/1475714273597/");
+ 			close.appendChild(img);
+ 			modal.appendChild(close);
+
+ 			close.addEventListener("click", function(){
+ 				modal.classList.add("hide");
+
+ 			});
+ 		});
+});
+ })();
